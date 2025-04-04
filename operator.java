c989f11b7bda73ce36044f131ca9fd344339f147
Sravanthi934 <sravanthidoppalapudi9346@gmail.com>
2025-04-04 15:45:02 +0530

    added operator.java file

---
+++ operator.java
@@ -0,0 +1,30 @@
+public class operator {
+    public static void main(String[] args){
+        int a= 10;
+        int b=15;
+        int sum; //declaration
+        int diff;
+        int product;
+        int div;
+        int modulo;
+        sum= a+b; //initilazation
+        diff= a-b;
+        product= a*b;
+        div= a/b;
+        modulo= a%b;
+        System.out.println("sum is " + sum);
+        System.out.println("difference is " + diff);
+        System.out.println("product is " + product);
+        System.out.println("division is " + div);
+        System.out.println("modulo div is " + modulo);
+        sum +=5; //sum = sum + 5;
+        System.out.println("sum is now " + sum);
+        sum ++;
+        System.out.println("sum is " + sum++);
+        sum--;
+        System.out.println("sum is " + sum--);
+        product *= 2;
+        System.out.println("product is " + product);
+        
+    }
+}
