0889bea6d6354e3c2d4899e31ff1b72774e27509
anilkumar <anilgithubd@gmail.com>
2025-04-04 11:09:01 +0530

    first commit

---
+++ comment.java
@@ -0,0 +1,8 @@
+public class comment {
+    /* main is the entry point to the application
+     */
+    public static void main(String[] args) //main is the entry point to the application 
+    {
+        System.out.println("hellohellohellohellohello");
+    }    
+}
---
+++ variable.java
@@ -0,0 +1,6 @@
+public class variable {
+    public static void main(String[] args) {
+       String myhometown ="chilakluripet";
+        System.out.println( myhometown);
+    }
+}
